9daaa17b29516ac560a177306dfc66b427f16ed3
kou7306 <kouta.1206.y@icloud.com>
2023-11-30 00:10:29 +0900

    マッチング

--- dbTest.py
+++ dbTest.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials,firestore
 
-cred = credentials.Certificate("firestoreTest/key.json")
+cred = credentials.Certificate("key.json")
 
 firebase_admin.initialize_app(cred)
 db = firestore.client()
