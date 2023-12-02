0fcd8924dc61220850326aee3cc19de3354c6f49
Masato Yamamoto <masato.yamamoto99@gmail.com>
2023-12-03 00:53:05 +0900

    slider機能追加しました

--- static/javascript/home.js
+++ static/javascript/home.js
@@ -1,20 +1,21 @@
-let currentIndex = 0;
-const cardsContainer = document.querySelector('.cards-container');
-const cards = document.querySelectorAll('.card');
-const cardWidth = cards[0].offsetWidth + 10; // カードの幅 + マージン
-
-function showCards() {
-  cardsContainer.style.transform = `translateX(${-currentIndex * cardWidth}px)`;
-}
-
-function nextCard() {
-  currentIndex = (currentIndex + 1) % cards.length;
-  showCards();
-}
-
-function prevCard() {
-  currentIndex = (currentIndex - 1 + cards.length) % cards.length;
-  showCards();
-}
-
-showCards();
+var swiper = new Swiper('.mySwiper', {
+     effect: 'coverflow',
+     slidesPerView: 2,
+     breakpoints: {
+          // 画面幅が630px以上のとき
+          630: {
+            slidesPerView: 5,
+          },
+     },
+     spaceBetween: 0,
+     grabCursor: true,
+     centeredSlides: true,
+     coverflowEffect: {
+          rotate: 15,
+          stretch: 0,
+          depth: 300,
+          modifier: 1,
+          slideShadows: true,
+     },
+     loop: true,
+});
