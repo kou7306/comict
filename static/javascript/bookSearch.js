b9a9bbd34fee6171756ffbdae9057dc6f3eee624
kohei-irohasu <yktmd059@icloud.com>
2024-02-04 18:13:09 +0900

    #sort

--- static/javascript/bookSearch.js
+++ static/javascript/bookSearch.js
@@ -1,25 +1,41 @@
 let page = 1;
 
 window.addEventListener('scroll', () => {
-    if (this.scrollTop + this.clientHeight >= (this.scrollHeight) * 0.8) {
+    let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    let clientHeight = document.documentElement.clientHeight;
+    let scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
+
+    if (scrollTop + clientHeight >= scrollHeight * 0.8) {
         searchBooks(page);
         page++;
     }
 });
 
+let currentResults = [];
+
 function searchBooks(page) {
+    const searchType = document.getElementById('searchType').value;
     const searchTerm = document.getElementById('searchInput').value.trim().toLowerCase();
+    const sortOption = document.querySelector('select[name="sortOption"]').value;
 
     if (searchTerm !== '') {
         $.ajax({
             type: 'POST',
             url: '/<user_id>/bookSearch',
             data: { 
+                searchType: searchType,
                 searchInput: searchTerm,
+                sortOption: sortOption,
                 page: page
             },
             success: (data) => {
-                displaySearchResults(data);
+                const appendResults = page > 1;
+                displaySearchResults(data, appendResults);
+                if (appendResults) {
+                    currentResults.results = currentResults.results.concat(data.results);
+                } else {
+                    currentResults = data;
+                }
             },
             error: (error) => {
                 console.error('検索エラー:', error);
@@ -30,29 +46,31 @@ function searchBooks(page) {
     }
 }
 
-function displaySearchResults(response) {
+function displaySearchResults(response, append=false) {
 
     const results = response.results;
     const numResults = response.num_results;
-    const user_id = response.user_id
+    const user_id = response.user_id;
 
     const resultsList = document.getElementById('searchResults');
+    if (!append) {
         resultsList.innerHTML = '';
+    }
 
-        if ('error' in results) {
-            alert(results.error);
-        } else {
-            results.forEach(result => {
-                const listItem = document.createElement('li');
-                const link = document.createElement('a');
+    if ('error' in results) {
+        alert(results.error);
+    } else {
+        results.forEach(result => {
+            const listItem = document.createElement('li');
+            const link = document.createElement('a');
 
-                link.href = `/${user_id}/${result.title}/detail`;
-                link.textContent = result.title;
-                
-                listItem.appendChild(link);
-                resultsList.appendChild(listItem);
-            });
-        }
+            link.href = `/${user_id}/${result.id}/detail`;
+            link.textContent = `${result.id} - ブックマーク数: ${result.bookmark.length}, レビュー数: ${result.reviews.length}`;
+            
+            listItem.appendChild(link);
+            resultsList.appendChild(listItem);
+        });
+    }
 
     const messageEl = document.getElementById('message');
     if(numResults === 0) {
