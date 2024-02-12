2aaf9bcc3123f02ea1b3f0b1a870875346f99da9
kou7306 <kouta.1206.y@icloud.com>
2024-02-12 16:46:35 +0900

    ユーザー詳細ページ

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ from pages.comic import comic_bp
 from pages.reviewAdd import reviewAdd_bp
 from pages.reviewLikes import reviewLikes_bp
 from pages.update_matching import update_matching_bp
+from pages.user import user_bp
 
 
 app = Flask(__name__)
@@ -41,7 +42,7 @@ app.register_blueprint(comic_bp)
 app.register_blueprint(reviewAdd_bp)  
 app.register_blueprint(reviewLikes_bp)  
 app.register_blueprint(update_matching_bp)
-
+app.register_blueprint(user_bp)
 
 app.secret_key = "secret"
 
