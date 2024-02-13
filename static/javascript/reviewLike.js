f463f5c467d0b403dc36a1800204839bef47e2f1
kou7306 <kouta.1206.y@icloud.com>
2024-02-13 17:39:41 +0900

    loginにリダイレクトするときの処理の修正

--- static/javascript/reviewLike.js
+++ static/javascript/reviewLike.js
@@ -8,28 +8,31 @@ function toggleLike(reviewId) {
     })
     .then(response => {
         if (response.status === 401) {
-            window.location.href = "/login?query=review";
+            window.location.href = "/login?query=review_detail?review_id=" + reviewId;
         } else if (!response.ok) {
             throw new Error('Response was not ok');
         }
         return response.json();
     })
     .then(data => {
-        const likeCountElement = document.querySelector(`#likeCount-${reviewId}`);
-        let likeCount = parseInt(likeCountElement.textContent.replace('いいね数: ', ''), 10);
-
-        const likeButton = document.querySelector(`#likeButton-${reviewId}`);
-
-        if (data.status === 'liked') {
-            likeCount += 1
-            likeButton.textContent = 'いいねを取り消す';
-        } else {
-            likeCount -= 1;
-            likeButton.textContent = 'いいね';
+        if (data.error != "Unauthorized") {
+            const likeCountElement = document.querySelector(`#likeCount-${reviewId}`);
+            let likeCount = parseInt(likeCountElement.textContent.replace('いいね数: ', ''), 10);
+    
+            const likeButton = document.querySelector(`#likeButton-${reviewId}`);
+    
+            if (data.status === 'liked') {
+                likeCount += 1
+                likeButton.textContent = 'いいねを取り消す';
+            } else {
+                likeCount -= 1;
+                likeButton.textContent = 'いいね';
+            }
+    
+            likeCountElement.textContent = `いいね数: ${likeCount}`;
+            console.log('Success:', data);
         }
 
-        likeCountElement.textContent = `いいね数: ${likeCount}`;
-        console.log('Success:', data);
     })
     .catch((error) => {
         console.error('Error:', error);
