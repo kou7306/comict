3791f5bfe230f1067244a4bf3958d325ec2b2fcf
kou7306 <kouta.1206.y@icloud.com>
2024-02-21 18:07:46 +0900

    edit-username

--- pages/userpage.py
+++ pages/userpage.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, session, flash, url_for, get_flashed_messages
+from flask import Blueprint, render_template, request, redirect, session, flash, url_for, get_flashed_messages,jsonify
 from firebaseSetUp import auth, db
 from bs4 import BeautifulSoup
 import requests
@@ -9,76 +9,84 @@ user_doc_ref = db.collection('user')
 review_doc_ref=db.collection('review')
 
 # ユーザーページ
-@userpage_bp .route('/userpage', methods=['GET', 'POST'])
+@userpage_bp.route('/userpage', methods=['GET', 'POST'])
 def user_page():   
-    user_id = session.get('user_id')
-    if not user_doc_ref.document(user_id).get().exists:
-        return redirect("/login?query=userpage")
-    if not user_id:
-        return redirect('/login?query=userpage')
-    user_id = session.get('user_id')
-    if user_id:
-        logged_in = True
-    else:
-        logged_in = False   
-    user_doc = user_doc_ref.document(user_id)
-    user=user_doc.get()
-    user_data=user.to_dict()
-    username=user_data["username"]
-    # 特定のユーザーネームに一致するドキュメントを取得
-    query = review_doc_ref.where('username', '==', username).get()
+    if request.method == 'GET': 
+        user_id = session.get('user_id')
+        if not user_doc_ref.document(user_id).get().exists:
+            return redirect("/login?query=userpage")
+        if not user_id:
+            return redirect('/login?query=userpage')
+        user_id = session.get('user_id')
+        if user_id:
+            logged_in = True
+        else:
+            logged_in = False 
 
-    # アンケート結果の取得・表示
-    genre_value=user_data.get("genre")
-    start_question = 20 * (int(genre_value) - 1)
-    end_question = start_question + 20
 
-    html_file_path=f"templates/question{genre_value}.html"
-    with open(html_file_path, 'r', encoding='utf-8') as file:
-        html_code = file.read()
-    result = user_data.get('mangaAnswer')
+     
+        user_doc = user_doc_ref.document(user_id)
+        user=user_doc.get()
+        user_data=user.to_dict()
+        username=user_data["username"]
+        # 特定のユーザーネームに一致するドキュメントを取得
+        query = review_doc_ref.where('username', '==', username).get()
 
-    #アンケートの設問を格納する配列
-    question = []
-    soup = BeautifulSoup(html_code, 'html.parser')
+        # アンケート結果の取得・表示
+        genre_value=user_data.get("genre")
+        start_question = 20 * (int(genre_value) - 1)
+        end_question = start_question + 20
 
-    #アンケートの設問を取得
-    h2_elems = soup.find_all('h2')
-    for h2 in h2_elems:
-        question.append(h2.text)
-    
-    #アンケートの回答を取得
-    answer = result[start_question:end_question]
+        html_file_path=f"templates/question{genre_value}.html"
+        with open(html_file_path, 'r', encoding='utf-8') as file:
+            html_code = file.read()
+        result = user_data.get('mangaAnswer')
 
-    #設問と回答をタプル化
-    combined_list = zip(question, answer)
+        #アンケートの設問を格納する配列
+        question = []
+        soup = BeautifulSoup(html_code, 'html.parser')
 
-    #選択したジャンルを取得
-    genre_list = ["バトル", "スポーツ", "恋愛", "ミステリー", "コメディ", "SF", "歴史"]
-    genre_choice = genre_list[int(genre_value)-1]
+        #アンケートの設問を取得
+        h2_elems = soup.find_all('h2')
+        for h2 in h2_elems:
+            question.append(h2.text)
+        
+        #アンケートの回答を取得
+        answer = result[start_question:end_question]
 
-    #ブックマークをデータベースから取得
-    favorite_titles = user_data["bookmark"]
+        #設問と回答をタプル化
+        combined_list = zip(question, answer)
 
-    # フォローしたユーザーのIDを取得
-    follow_data = []
-    for follow_id in user_data["follow"]:
-        follow_doc = user_doc_ref.document(follow_id).get()
-        if follow_doc.exists:
-            follow_name = follow_doc.to_dict()["username"]
-            follow_data.append((follow_name, follow_id))
+        #選択したジャンルを取得
+        genre_list = ["バトル", "スポーツ", "恋愛", "ミステリー", "コメディ", "SF", "歴史"]
+        genre_choice = genre_list[int(genre_value)-1]
 
-    return render_template("userpage.html", myreview_query=query,username=username, user_id=user_id,favorite_titles=favorite_titles,follow_data=follow_data,result=result, combined_list=combined_list, genre_choice=genre_choice, logged_in=logged_in)
+        #ブックマークをデータベースから取得
+        favorite_titles = user_data["bookmark"]
 
-@userpage_bp.route('/userpage/<id>', methods=['GET', 'POST'])
-def update(id):
-    if 'user' in session:
-        name = request.form['username']
-        print(name)
-        try:
-            db.collection('user').document(id).update({'username': name})
-            return redirect('/userpage')
-        except:
-            return redirect('/userpage', code=500)
-    else:
-        return redirect('/login?query=userpage')
+        # フォローしたユーザーのIDを取得
+        follow_data = []
+        for follow_id in user_data["follow"]:
+            follow_doc = user_doc_ref.document(follow_id).get()
+            if follow_doc.exists:
+                follow_name = follow_doc.to_dict()["username"]
+                follow_data.append((follow_name, follow_id))
+
+
+        follow_num = len(follow_data)
+
+        return render_template("userpage.html", myreview_query=query,username=username, user_id=user_id,favorite_titles=favorite_titles,follow_data=follow_data,result=result, combined_list=combined_list, genre_choice=genre_choice, logged_in=logged_in,follow_num=follow_num)
+ 
+
+@userpage_bp.route('/edit/<user_id>', methods=['POST']) 
+def edit_name(user_id): 
+    data = request.json
+
+    new_username = data.get("username")
+    print(new_username)
+    print(user_id)
+    user_doc_ref.document(user_id).update({"username": new_username})
+    return jsonify({"status": "success"})
+
+
+
