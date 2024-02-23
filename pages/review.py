138df317f694d74bfdf2f30f534944f546f78068
kappy <shangenjunfu9@gmail.com>
2024-02-24 05:06:55 +0900

    reviewLike

--- pages/review.py
+++ pages/review.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, session, flash, url_for, get_flashed_messages
+from flask import Blueprint, render_template, request, redirect, session, flash, url_for, get_flashed_messages, jsonify
 from firebaseSetUp import auth, db
 from funcs.wiki import get_manga_title,get_wikipedia_page_details
 from firebase_admin import credentials, firestore
@@ -12,7 +12,7 @@ review_doc_ref=db.collection('review')
 comics_doc_ref=db.collection('comics')
 
 # レビュー一覧
-@review_bp.route('/review')
+@review_bp.route('/review', methods=['GET', 'POST'])
 def review():
     if request.method == 'GET':
         user_id = session.get('user_id')
@@ -27,7 +27,25 @@ def review():
 
 
         reviews = review_sort(sort_option,None)
-
         return render_template("review.html", user_id=user_id, reviews=reviews, sort_option=sort_option, logged_in=logged_in, user_doc_ref=user_doc_ref, comics_doc_ref=comics_doc_ref)
+
+    elif request.method == 'POST':
+        user_id = session.get('user_id')
+        review_id = request.form.get('review_id')
+        review_ref = db.collection('review').document(review_id)
+        review_doc = review_ref.get()
+    
+        if review_doc.exists:
+            likes = review_doc.to_dict().get('likes', [])
+            if user_id in likes:
+                review_ref.update({'likes': firestore.ArrayRemove([user_id])})
+                review_ref.update({'likes_count': firestore.Increment(-1)})
+                return jsonify({'status': 'unliked'}), 200
+            else:
+                review_ref.update({'likes': firestore.ArrayUnion([user_id])})
+                review_ref.update({'likes_count': firestore.Increment(1)})
+                return jsonify({'status': 'liked'}), 200
+        else:
+            return jsonify({"error": "Review not found"}), 404
 
 
