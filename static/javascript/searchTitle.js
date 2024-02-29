f58cac89a79bf15886d2b3206418f8e67829f3f7
kou7306 <kouta.1206.y@icloud.com>
2024-02-29 16:45:56 +0900

    トップUI

--- static/javascript/searchTitle.js
+++ static/javascript/searchTitle.js
@@ -1,7 +1,9 @@
 async function searchTitle() {
-
+    var overlay = document.getElementById("overlay");
+    overlay.style.display = "block";
+    document.getElementById('loadingIndicator').style.display = 'flex';
     var title = document.getElementById('searchInput').value;
-  
+    
     // titleをAPIに渡す処理をここに記述する
     //　正式名称に変換
 
@@ -12,11 +14,14 @@ async function searchTitle() {
     var selectedTitle = data.manga_title;
     var is_exist = data.is_exist;
     if (selectedTitle == null || is_exist == false) {
+        document.getElementById('loadingIndicator').style.display = 'none';
         // 検索結果が空の場合は処理しない
         alert('作品がありませんでした');
+        
         return;
     }
     else {
+        document.getElementById('loadingIndicator').style.display = 'none';
         // 選択された作品名を検索欄に入力
         document.getElementById('searchInput').value = "";
         // 漫画のページに遷移
